01d9771dc28a0da7e967171b16852577be73ad5e
tymowskyy <tymowskyy@gmail.com>
2023-01-02 20:50:32 +0100

    added Chunk and ChunkRenderer

--- Assets/Scripts/ChunkRenderer.cs
+++ Assets/Scripts/ChunkRenderer.cs
@@ -5,57 +5,106 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class ChunkRenderer : MonoBehaviour
 {
-    Mesh mesh;
-    Vector3[] vertices;
-    int[] triangles;
+    private Mesh mesh;
+    private List<Vector3> vertices;
+    private List<int> triangles;
 
-    // Start is called before the first frame update
-    void Start()
+    public void OnStart()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        UpdateChunk();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateChunk(ref int[,,] blocks)
     {
-        
-    }
-    public void UpdateChunk()
-    {
-        vertices = new Vector3[(Settings.CHUNK_SIZE + 1) * (Settings.CHUNK_SIZE + 1)];
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
 
-        for (int x = 0; x <= Settings.CHUNK_SIZE; ++x)
+        for (int x = 0; x < Settings.CHUNK_WIDTH; ++x)
         {
-            for (int z = 0; z <= Settings.CHUNK_SIZE; ++z)
+            for (int y = 0; y < Settings.CHUNK_HEIGHT; ++y)
             {
-                float y = Random.Range(0f, 1f);
-                vertices[x * (Settings.CHUNK_SIZE + 1) + z] = new Vector3(x, y, z);
+                for (int z = 0; z < Settings.CHUNK_WIDTH; ++z)
+                {
+                    UpdateBlock(x, y, z, ref blocks);
+                }
             }
         }
 
-        triangles = new int[6 * Settings.CHUNK_SIZE * Settings.CHUNK_SIZE];
-
-        for (int x=0; x < Settings.CHUNK_SIZE; ++x)
-        {
-            for (int z=0; z < Settings.CHUNK_SIZE; ++z)
-            {
-                int triange_index = 6 * (x * Settings.CHUNK_SIZE + z);
-
-                triangles[triange_index + 0] = x * (Settings.CHUNK_SIZE + 1) + z;
-                triangles[triange_index + 1] = x * (Settings.CHUNK_SIZE + 1) + z + 1;
-                triangles[triange_index + 2] = (x+1) * (Settings.CHUNK_SIZE + 1) + z;
-
-                triangles[triange_index + 3] = x * (Settings.CHUNK_SIZE + 1) + z + 1;
-                triangles[triange_index + 4] = (x+1) * (Settings.CHUNK_SIZE + 1) + z + 1;
-                triangles[triange_index + 5] = (x+1) * (Settings.CHUNK_SIZE + 1) + z;
-                    
-            }
-        }
-
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals();
     }
+
+    private void UpdateBlock(int x, int y, int z, ref int[,,] blocks)
+    {
+        if (!Chunk.IsInChunk(x, y, z) ||
+            blocks[x, y, z] == 0) return;
+
+        Vector3 vertex = new Vector3(x, y, z);
+        if (Chunk.IsInChunk(x - 1, y, z) && blocks[x - 1, y, z] == 0)
+            UpdateFace(vertex, new Vector3[] {
+                new Vector3(0, 0, 0),
+                new Vector3(0, 0, 1),
+                new Vector3(0, 1, 0),
+                new Vector3(0, 1, 1),
+            });
+        if (Chunk.IsInChunk(x + 1, y, z) && blocks[x + 1, y, z] == 0)
+            UpdateFace(vertex, new Vector3[] {
+                new Vector3(1, 0, 0),
+                new Vector3(1, 1, 0),
+                new Vector3(1, 0, 1),
+                new Vector3(1, 1, 1),
+            });
+
+        if (Chunk.IsInChunk(x, y-1, z) && blocks[x, y-1, z] == 0)
+            UpdateFace(vertex, new Vector3[] {
+                new Vector3(0, 0, 0),
+                new Vector3(1, 0, 0),
+                new Vector3(0, 0, 1),
+                new Vector3(1, 0, 1),
+            });
+
+        if (Chunk.IsInChunk(x, y + 1, z) && blocks[x, y + 1, z] == 0)
+            UpdateFace(vertex, new Vector3[] {
+                new Vector3(0, 1, 0),
+                new Vector3(0, 1, 1),
+                new Vector3(1, 1, 0),
+                new Vector3(1, 1, 1),
+            });
+
+        if (Chunk.IsInChunk(x, y, z - 1) && blocks[x, y, z - 1] == 0)
+            UpdateFace(vertex, new Vector3[] {
+                new Vector3(0, 0, 0),
+                new Vector3(0, 1, 0),
+                new Vector3(1, 0, 0),
+                new Vector3(1, 1, 0),
+            });
+
+        if (Chunk.IsInChunk(x, y, z + 1) && blocks[x, y, z + 1] == 0)
+            UpdateFace(vertex, new Vector3[] {
+                new Vector3(0, 0, 1),
+                new Vector3(1, 0, 1),
+                new Vector3(0, 1, 1),
+                new Vector3(1, 1, 1),
+            });
+    }
+
+    private void UpdateFace(Vector3 vertex, Vector3[] vertices_offsets)
+    {
+        int index = vertices.Count;
+
+        foreach (Vector3 vertex_offset in vertices_offsets)
+            vertices.Add(vertex + vertex_offset);
+
+        triangles.AddRange(new int[] {
+            index + 0,
+            index + 1,
+            index + 2,
+            index + 2,
+            index + 1,
+            index + 3,
+        });
+    }
 }
+ 
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,4 +1,5 @@
 public static class Settings 
 {
-    public const int CHUNK_SIZE = 16;
+    public const int CHUNK_WIDTH = 16;
+    public const int CHUNK_HEIGHT = 64;
 }
