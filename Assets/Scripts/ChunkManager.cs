afac6a6bd4bc66d0badd5233fbe7951b0af3e22d
tymowskyy <tymowskyy@gmail.com>
2023-01-05 18:15:22 +0100

    ChunkManager now stores Chunk components instead of GameObjects

--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -6,15 +6,13 @@ using UnityEngine;
 public class ChunkManager : MonoBehaviour
 {
     public GameObject chunkPrefab;
-    private Dictionary<Vector2, GameObject> chunks;
-    private readonly Block[,,] emptyChunk = new Block[Settings.CHUNK_WIDTH, Settings.CHUNK_HEIGHT, Settings.CHUNK_WIDTH];
+    private Dictionary<Vector2, Chunk> chunks;
     private TerrainGenerator terrainGenerator;
 
-    // Start is called before the first frame update
     void Start()
     {
         terrainGenerator = GetComponent<TerrainGenerator>();
-        chunks = new Dictionary<Vector2, GameObject>();
+        chunks = new Dictionary<Vector2, Chunk>();
         GenerateWorld();
         RenderWorld();
     }
@@ -32,29 +30,26 @@ public class ChunkManager : MonoBehaviour
 
     private void GenerateChunk(int chunkX, int chunkZ)
     {
-        GameObject chunk = Instantiate(
+        Chunk chunk = Instantiate(
             chunkPrefab,
             new Vector3(Settings.CHUNK_WIDTH * chunkX, 0, Settings.CHUNK_WIDTH * chunkZ),
             Quaternion.identity,
             transform
-        );
+        ).GetComponent<Chunk>();
         chunks.Add(
             new Vector2(chunkX, chunkZ),
             chunk
         );
 
-        chunk.GetComponent<Chunk>().blocks = terrainGenerator.GenerateChunk(chunkX, chunkZ);
-
+        chunk.blocks = terrainGenerator.GenerateChunk(chunkX, chunkZ);
     }
 
     private void RenderWorld()
     {
         foreach (var item in chunks)
         {
-            Chunk chunkComponent = item.Value.GetComponent<Chunk>();
-
-            chunkComponent.terrain.UpdateChunk(
-                chunkComponent.blocks,
+            item.Value.terrain.UpdateChunk(
+                item.Value.blocks,
                 GetChunkBlocks(item.Key + new Vector2(1, 0)),
                 GetChunkBlocks(item.Key + new Vector2(-1, 0)),
                 GetChunkBlocks(item.Key + new Vector2(0, 1)),
@@ -67,7 +62,7 @@ public class ChunkManager : MonoBehaviour
     {
         return
             chunks.ContainsKey(chunkPos) ?
-            chunks[chunkPos].GetComponent<Chunk>().blocks :
-            emptyChunk;
+            chunks[chunkPos].blocks :
+            new Block[Settings.CHUNK_WIDTH, Settings.CHUNK_HEIGHT, Settings.CHUNK_WIDTH];
     }
 }
